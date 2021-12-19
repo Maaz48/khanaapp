caa36721edae3a9c43e684deb8c1aeb01759ae12
Maaz48 <maazanis48@gmail.com>
2021-12-19 17:40:00 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Approve from "./comp/Approve";
+import Reject from "./comp/Reject";
+import Pending from "./comp/Pending";
+import { Tabs, Tab, Navbar, Container, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ADMIN DASHBOARD</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto"></Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          marginTop: 20,
+          border: 2,
+          borderStyle: "sold",
+          borderColor: "black",
+          paddingLeft: 50,
+          paddingRight: 50,
+        }}
+      >
+        <Tabs
+          defaultActiveKey="pending"
+          id="uncontrolled-tab-example"
+          className="mb-3"
         >
-          Learn React
-        </a>
-      </header>
+          <Tab eventKey="pending" title="pending">
+            <Pending />
+          </Tab>
+          <Tab eventKey="Approve" title="Approve">
+            <Approve />
+          </Tab>
+          <Tab eventKey="Reject" title="Reject">
+            <Reject />
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
---
+++ src/comp/Reject.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import {
+  auth,
+  db,
+  collection,
+  query,
+  where,
+  onSnapshot,
+} from "../firebase/firebase";
+import { Spinner, Card, Button } from "react-bootstrap";
+function Approve() {
+  const [loader, setloader] = useState(true);
+  const [state, setstate] = useState("");
+  var data;
+  useEffect(() => {
+    console.log("YAHA Hn m");
+    const q = query(
+      collection(db, "NeedyUSERAndMANAGER"),
+      where("registrationType", "==", "user")
+    );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      // const cities = [];
+      querySnapshot.forEach((doc) => {
+        //   cities.push(doc.data().name);
+        console.log(doc.data());
+        data = [];
+        data.push(doc.data());
+        setstate(data);
+        console.log("data check", state);
+      });
+      // console.log("Current cities in CA: ", cities.join(", "));
+    });
+  }, []);
+
+  return (
+    <div>
+      <>
+        {loader ? (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        ) : (
+          data.map((data) => {
+            <>
+              <Card>
+                <Card.Header>{data.fname}</Card.Header>
+                <Card.Body>
+                  <Card.Title>{data.email}</Card.Title>
+                  <Card.Text></Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </>;
+          })
+        )}
+      </>
+    </div>
+  );
+}
+
+export default Approve;
